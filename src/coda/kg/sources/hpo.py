627c6f9eca1e79050b519c3890d7d77484266350
kkaris <karis.klas@gmail.com>
2026-01-13 08:50:07 -0800

    Update hp exporting

--- src/coda/kg/sources/hpo.py
+++ src/coda/kg/sources/hpo.py
@@ -1,10 +1,10 @@
 import pandas as pd
-import networkx as nx
 import obonet
 
 from coda import CODA_BASE
+from coda.kg.sources import KGSourceExporter
 
-HPO_BASE = CODA_BASE.module('hpo')
+HPO_BASE = CODA_BASE.module("hpo")
 HPOA_URL = "https://purl.obolibrary.org/obo/hp/phenotype.hpoa"
 HPO_URL = "https://purl.obolibrary.org/obo/hp.obo"
 
@@ -12,59 +12,74 @@ HPO_URL = "https://purl.obolibrary.org/obo/hp.obo"
 #      6 OMIM:619340»Developmental and epileptic encephalopathy 96»··»···HP:0011097»·PMID:31675180»··PCS»»···1/2»»···»···P»··HPO:probinson[2021-06-21]
 #      7 OMIM:619340»Developmental and epileptic encephalopathy 96»··»···HP:0002187»·PMID:31675180»··PCS»»···1/1»»···»···P»··HPO:probinson[2021-06-21]
 
-def get_hpoa_graph():
-    hpoa_file = HPO_BASE.ensure(url=HPOA_URL)
-    hpo_file = HPO_BASE.ensure(url=HPO_URL)
-    df = pd.read_csv(hpoa_file, sep='\t', skiprows=4)
-    hp_graph = obonet.read_obo(hpo_file)
-    nodes = []
-    edges = []
-    disease_added = set()
-    phenotypes_added = set()
-    for _, row in df.iterrows():
-        disease_id = row['database_id']
-        disease_name = row['disease_name']
-        hpo_id = row['hpo_id']
 
-        # FIXME: check if this is actually always valid
-        disease_curie = disease_id.lower()
-        phenotype_curie = hpo_id.lower()
+class HpoExporter(KGSourceExporter):
+    name = "hpo"
 
-        if disease_curie not in disease_added:
-            disease_ns = disease_curie.split(':')[0]
-            nodes.append([
-                disease_curie, {
-                    'name': disease_name,
-                    'kind': disease_ns
-                }
-            ])
-            disease_added.add(disease_curie)
+    def export(self):
+        hpoa_file = HPO_BASE.ensure(url=HPOA_URL)
+        hpo_file = HPO_BASE.ensure(url=HPO_URL)
+        df = pd.read_csv(hpoa_file, sep="\t", skiprows=4)
 
-        if phenotype_curie not in phenotypes_added:
-            hp_term_name = hp_graph.nodes[hpo_id]['name']
-            nodes.append([
-                phenotype_curie, {
-                    'kind': 'hp',
-                    'name': hp_term_name,
-                }
-            ])
-            phenotypes_added.add(phenotype_curie)
+        # Set curies and types
+        df["disease_curie"] = df["database_id"].str.lower()
+        df["phenotype_curie"] = df["hpo_id"].str.lower()
+        df["disease_type"] = df["database_id"].str.split(":").str[0].str.lower()
+        df["phenotype_type"] = df["hpo_id"].str.split(":").str[0].str.lower()
 
-        edges.append((
-            disease_curie,
-            phenotype_curie,
-            {
-                'evidence': row['evidence'],
-                'frequency': row['frequency'],
-                'onset': row['onset'],
-                'kind': 'has_phenotype',
-                'qualifier': row['qualifier'],
-                'sex': row['sex'],
-                'aspect': row['aspect'],
-                'modifier': row['modifier'],
+        # Load HPO graph to get term names
+        hp_graph = obonet.read_obo(hpo_file)
+        df["hpo_name"] = df["hpo_id"].map(
+            lambda x: hp_graph.nodes[x]["name"] if x in hp_graph.nodes else None
+        )
+
+        # Dump disease and phenotype nodes
+        pd.concat(
+            [
+                df[["disease_curie", "disease_type", "disease_name"]].rename(
+                    columns={
+                        "disease_curie": "id:ID",
+                        "disease_type": ":LABEL",
+                        "disease_name": "name",
+                    }
+                ),
+                df[["phenotype_curie", "phenotype_type", "hpo_name"]].rename(
+                    columns={
+                        "phenotype_curie": "id:ID",
+                        "phenotype_type": ":LABEL",
+                        "hpo_name": "name",
+                    }
+                ),
+            ]
+        ).sort_values("id:ID").drop_duplicates().to_csv(
+            self.nodes_file, index=False, sep="\t"
+        )
+
+        # Dump edges
+        edges = df[
+            [
+                "disease_curie",
+                "phenotype_curie",
+                "evidence",
+                "frequency",
+                "onset",
+                "qualifier",
+                "sex",
+                "aspect",
+                "modifier",
+            ]
+        ]
+        edges = edges.rename(
+            columns={
+                "disease_curie": ":START_ID",
+                "phenotype_curie": ":END_ID",
             }
-        ))
-    g = nx.DiGraph()
-    g.add_nodes_from(nodes)
-    g.add_edges_from(edges)
-    return g
+        )
+        edges[":TYPE"] = "has_phenotype"
+        edges = edges.sort_values([":START_ID", ":END_ID"])
+        edges.drop_duplicates().to_csv(self.edges_file, sep="\t", index=False)
+
+
+if __name__ == "__main__":
+    exporter = HpoExporter()
+    exporter.export()
